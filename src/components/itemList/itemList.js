23b7a65bf418077190a694a69958a08be6951fd5
Fernando Altavista <fer.altavista5@gmail.com>
2021-05-03 10:22:14 -0300

    Add loader in ItemList

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,37 +1,55 @@
 import './itemList.css'
 import {Item} from '../item/item'
 import {useState,useEffect} from 'react'
+import {useParams} from 'react-router'
+import {Loader} from '../loader/loader'
 
-export const ItemList = ({list}) => {
-    
+
+export const ItemList = ({data}) => {
+
+const {categoryId} = useParams()
 const [items, setItems] = useState([])
+const [isLoading, setIsLoading] = useState(false)
+
+    const getCategoryId = (data)=>{
+    let itemsfind = data.filter(item => item.categoryId === categoryId)
+    return itemsfind
+    }
 
 const getList = ((data) =>{
     return new Promise((resolve)=> {
     setTimeout(() => {
-        return resolve(data)
-    }, 2000);
-    })
+        if (categoryId){
+            return resolve(getCategoryId(data))
+        } else{
+            return resolve(data)
+        }
+    }, 1500);
+})
 })
 
 useEffect(() => {
-    getList(list).then(result => {
-    setItems(result)
-    })
-}, [])
+        setIsLoading(true)
+        getList(data)
+        .then(result => setItems(result))
+        .finally(()=> setIsLoading(false))
+    
+}, [categoryId])
 
     return (
-    
+   
         <div className="itemList">
-            <div className="row">
-                {items && items.map(item => 
-                    
-                    <Item key={item.id} item={item}></Item>
-
-                    
-                )}
-                
-            </div>
+            {
+            categoryId ? (<h4 className="textCategory">Categoria: {categoryId}</h4>)
+            : (<h2 className="gretting">Bienvenido a nuestra tienda</h2>)
+            }
+            {
+            (isLoading) ? (<Loader />)
+                : <div className="row"> {
+                    items && items.map(item => 
+                    <Item key={item.id} item={item} />)} 
+                </div>
+            }   
         </div>
-    )
-}
+    
+)}
