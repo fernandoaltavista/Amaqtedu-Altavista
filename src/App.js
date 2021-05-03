4b3c1e7c8df65989a54c14cf9af0b96d6c35deea
Fernando Altavista <fer.altavista5@gmail.com>
2021-05-03 10:23:47 -0300

    Add routing,Switch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import data from './components/products/products.json'
 import {Navbar} from './components/navbar/navbar'
 import {ItemListContainer} from './components/itemListContainer/itemListContainer'
-import 'bootstrap/dist/css/bootstrap.css'
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import { ItemDetailContainer } from './components/itemDetailContainer/itemDetailContainer'
 
 function App() {
   const USER = {
@@ -8,11 +11,22 @@ function App() {
       image:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
   }
   return (
-    <div className="App">
+   
+      <BrowserRouter>
+        <Navbar user={USER} />
+        <Switch>
+          <Route exact path='/'>
+            <ItemListContainer data={data}/>
+          </Route>
+          <Route path='/category/:categoryId'>
+            <ItemListContainer data={data}/>
+          </Route>
+          <Route path='/item/:id'>
+            <ItemDetailContainer data={data} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
       
-      <Navbar user={USER} />
-      <ItemListContainer/>
-    </div>
   );
 }
 
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,26 +1,32 @@
 import './navbar.css'
 import logo from'./logo_peq.png'
+import {NavLink} from 'react-router-dom'
 import {CartWidget} from '../cartWidget/cartWidget'
+import {Login} from '../login/login'
 
-export const Navbar = (props) => {
+export const Navbar = ({user}) => {
     const COUNT = 3
-    return (
-        <header>
-            <nav className="navbar">
-                <img src={logo} className="Logo" alt=""/>
+return (
+    <header>
+        <nav className="navbar">
+            <NavLink exact to='/'><img src={logo} className="Logo" alt=""/></NavLink>
                 
-                <ul>
-                    <li className="itemNav"><a href="#home">Home</a></li>
-                    <li className="itemNav"><a href="#lienzos">Lienzos</a></li>
-                    <li className="itemNav"><a href="#remera">Remeras</a></li>
-                    <li className="itemNav"><a href="#contact">Contacto</a></li>
+            <ul className="sidebar">
+                <li><NavLink exact to='/' className="itemNav" activeClassName="selectedLink">Home</NavLink></li>
+                <li><NavLink to='/category/lienzos' activeClassName="selectedLink" className="itemNav">Lienzos</NavLink></li>
+                <li><NavLink to='/category/remeras' activeClassName="selectedLink" className="itemNav">Remeras</NavLink></li>
+                <li><NavLink to='/contact' className="itemNav" activeClassName="selectedLink">Contacto</NavLink></li>
                     
-                </ul>
+            </ul>
+            <div className="loginContainer">
                 <CartWidget count={COUNT}/>
-                
-                <img src={props.user.image} className="userImage"alt="User"></img>
-            </nav>
-        </header>
+                <Login user= {user}/>
+                <span className="icon-toggler"></span>
+
+            </div>
+            
+        </nav>
+    </header>
         
         
     )
