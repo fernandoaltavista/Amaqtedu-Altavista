a8ca1e0e207871bacc9dc078e21f8c726d18e41e
Fernando Altavista <fer.altavista5@gmail.com>
2021-05-23 23:50:23 -0300

    test navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,16 +2,18 @@ import './navbar.css'
 import {CartWidget} from '../cartWidget/cartWidget'
 import {Login} from '../login/login'
 import {NavLink} from 'react-router-dom'
+import {useState} from 'react'
 import logo from'../../assets/images/svg/logo_peq.png'
 
-export const Navbar = ({user}) => {
+export const Navbar = () => {
+const [isOpen, setIsOpen] = useState(true)
 
 return (
     <header>
         <nav className="navbar">
             <NavLink exact to='/'><img src={logo} className="Logo" alt=""/></NavLink>
                 
-            <ul className="sidebar">
+            <ul className={isOpen ? "sidebar" : "active"}>
                 <li><NavLink exact to='/' className="itemNav" activeClassName="selectedLink">Home</NavLink></li>
                 <li><NavLink to='/category/lienzos' activeClassName="selectedLink" className="itemNav">Lienzos</NavLink></li>
                 <li><NavLink to='/category/remeras' activeClassName="selectedLink" className="itemNav">Remeras</NavLink></li>
@@ -21,6 +23,7 @@ return (
             <div className="loginContainer">
                 <NavLink to='/cart' className="itemNav"><CartWidget /></NavLink>
                 <Login/>
+
                 <span className="icon-toggler"></span>
 
             </div>
