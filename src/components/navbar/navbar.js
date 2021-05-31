90bbafe65ab1ce5b25c2243a20491caabede1800
Fernando Altavista <fer.altavista5@gmail.com>
2021-05-31 17:26:44 -0300

    Navbar responsive and map category

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,34 +1,63 @@
 import './navbar.css'
 import {CartWidget} from '../cartWidget/cartWidget'
-import {Login} from '../login/login'
 import {NavLink} from 'react-router-dom'
 import {useState} from 'react'
+import category from '../../service/category.json'
 import logo from'../../assets/images/svg/logo_peq.png'
 
 export const Navbar = () => {
-const [isOpen, setIsOpen] = useState(true)
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeClick = ()=> setClick(false)
 
 return (
     <header>
         <nav className="navbar">
-            <NavLink exact to='/'><img src={logo} className="Logo" alt=""/></NavLink>
+            <NavLink exact to='/'><img src={logo} 
+                    className="Logo" alt=""/>
+            </NavLink>
                 
-            <ul className={isOpen ? "sidebar" : "active"}>
-                <li><NavLink exact to='/' className="itemNav" activeClassName="selectedLink">Home</NavLink></li>
-                <li><NavLink to='/category/lienzos' activeClassName="selectedLink" className="itemNav">Lienzos</NavLink></li>
-                <li><NavLink to='/category/remeras' activeClassName="selectedLink" className="itemNav">Remeras</NavLink></li>
-                <li><NavLink to='/contact' className="itemNav" activeClassName="selectedLink">Contacto</NavLink></li>
+            <ul className={click ? "sidebar active" : "sidebar"}>
+                <li onClick={closeClick}>
+                    <NavLink exact to='/' className="itemNav" 
+                    activeClassName="selectedLink">
+                        Home
+                    </NavLink>
+                </li>
+
+            { category.map(({id,categoryName,route}) =>
+                <li key={id} onClick={closeClick}>
+                    <NavLink to={route} 
+                        className="itemNav"
+                        activeClassName="selectedLink" >
+                            {categoryName}
+                    </NavLink>
+                </li>)
+            }
+
+                <li onClick={closeClick}>
+                    <NavLink to='/contact' className="itemNav" 
+                    activeClassName="selectedLink">
+                        Contacto
+                    </NavLink>
+                </li>
                     
             </ul>
+
             <div className="loginContainer">
                 <NavLink to='/cart' className="itemNav"><CartWidget /></NavLink>
-                <Login/>
-
-                <span className="icon-toggler"></span>
+                <div className="mobile-menu" onClick={handleClick}>
+                    {click ? 
+                        <span className="icon-close"></span> 
+                        :
+                        <span className="icon-toggler" ></span>}
+                </div>
 
             </div>
             
         </nav>
+
     </header>
         
         
