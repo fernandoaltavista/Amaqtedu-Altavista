e10b5326d85ed72c3c3d867e457593e0954ff957
Fernando Altavista <fer.altavista5@gmail.com>
2021-05-31 17:31:24 -0300

    modification Loader

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -1,18 +1,24 @@
 import './loader.css'
 
-export const Loader = () => {
+export const Loader = (props) => {
 
     return (
             <div className="loader">
-                <div className="sk-chase">
-                    <div className="sk-chase-dot"></div>
-                    <div className="sk-chase-dot"></div>
-                    <div className="sk-chase-dot"></div>
-                    <div className="sk-chase-dot"></div>
-                    <div className="sk-chase-dot"></div>
-                    <div className="sk-chase-dot"></div>
-                </div>
-                <p className="textLoader">Recopilando datos de servidor</p>
+            <div className="sk-fading-circle">
+                <div className="sk-circle1 sk-circle"></div>
+                <div className="sk-circle2 sk-circle"></div>
+                <div className="sk-circle3 sk-circle"></div>
+                <div className="sk-circle4 sk-circle"></div>
+                <div className="sk-circle5 sk-circle"></div>
+                <div className="sk-circle6 sk-circle"></div>
+                <div className="sk-circle7 sk-circle"></div>
+                <div className="sk-circle8 sk-circle"></div>
+                <div className="sk-circle9 sk-circle"></div>
+                <div className="sk-circle10 sk-circle"></div>
+                <div className="sk-circle11 sk-circle"></div>
+                <div className="sk-circle12 sk-circle"></div>
+            </div>
+                <p className="textLoader">{props.text}</p>
             </div>
     )
 }
--- src/components/login/login.js
+++
@@ -1,9 +0,0 @@
-import './login.css'
-
-export const Login = () => {
-    return (
-        <div className="login">
-           <button className="buttonLoginIn">Login In</button>
-        </div>
-    )
-}
